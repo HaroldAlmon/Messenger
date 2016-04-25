9d8d274563dc5a9b18845090dc0abe9be4399c62
haroldalmon <HaroldAlmon@users.noreply.github.com>
2016-04-25 14:17:57 -0500

    Add context functon

--- src/main/java/com/translationdata/messenger/resources/ParameterResource.java
+++ src/main/java/com/translationdata/messenger/resources/ParameterResource.java
@@ -7,7 +7,10 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 @Path("parameter")
 @Consumes(MediaType.TEXT_PLAIN)
@@ -16,9 +19,17 @@ import javax.ws.rs.core.MediaType;
 public class ParameterResource {
 	@GET
 	@Path("/annotations")
-	public String getParamsUsinsAnnotations(@MatrixParam("size") int size,
+	public String getParamsUsingAnnotations(@MatrixParam("size") int size,
 											@CookieParam("name") String name,
 											@HeaderParam("priorityLevel") String priority) {
 		return "Test Params by Annotation.\nSize = " + size + "\nName (Cookie Parameter) = " + name + "\nPriority (Header Parameter)= " + priority;
+	}
+
+	@GET
+	@Path("/context")
+	public String getParamsUsingAnnotations(@Context UriInfo uriInfo, @Context HttpHeaders headers) {
+		return "UriInfo: " + uriInfo.getAbsolutePath() 
+				+ "\nHeaders = " + headers.getRequestHeaders().toString()
+				+ "\nCookies = " + headers.getCookies().toString();
 	}
 }
