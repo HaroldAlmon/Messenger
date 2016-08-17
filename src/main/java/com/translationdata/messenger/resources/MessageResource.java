12a1c9cca820c1aff41e397e4a0aa7ac25cfa856
haroldalmon <HaroldAlmon@users.noreply.github.com>
2016-08-17 18:47:35 -0500

    Delete import

--- src/main/java/com/translationdata/messenger/resources/MessageResource.java
+++ src/main/java/com/translationdata/messenger/resources/MessageResource.java
@@ -11,14 +11,13 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.translationdata.messenger.model.Message;
 import com.translationdata.messenger.resources.beans.MessageFilterBean;
 import com.translationdata.messenger.service.MessageService;
 
-// Maps to http://localhost:8080/messenger/webapi/messages.
+// Maps to http://localhost:8080/messenger/webapi/messages
 @Path("/messages")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
